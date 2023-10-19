984a653536daf402d0779e83ef4da14d3a0a47d2
Gisele Araujo Silva <gisele.araujosilva-ext@ab-inbev.com>
2023-10-19 16:45:20 -0300

    :sparkles: Added new text in tips pages

--- src/pages/Tips/index.jsx
+++ src/pages/Tips/index.jsx
@@ -1,7 +1,7 @@
 export const Tips = () => {
   return (
     <div>
-      <h1>Tips</h1>
+      <h1 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50rem'}}>Em progresso...</h1>
     </div>
   );
 }
