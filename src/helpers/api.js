c61f286e4530e1f448c679bfadfacc71b897745d
Gisele Araujo Silva <gisele.araujosilva-ext@ab-inbev.com>
2023-10-20 11:01:07 -0300

    :recycle: Refactored api and get for patients

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,5 +5,3 @@ export const apiBaseUrl = import.meta.env.VITE_APP_API_URL;
 export const api = axios.create({
   baseURL: apiBaseUrl,
 });
-
-export const feacher = (url) => api.get(url).then((res) => res.data);
--- src/pages/Patients/index.jsx
+++ src/pages/Patients/index.jsx
@@ -3,20 +3,40 @@ import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOu
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import * as Styled from "./Patients.styles";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ModalForm } from '../../components/ModalForm';
-import useSWR from 'swr';
-import { api, apiBaseUrl, feacher } from '../../helpers/api';
-import { utcToZonedTime, format } from 'date-fns-tz';
+import { api, apiBaseUrl} from '../../helpers/api';
 import { Button, Modal } from 'rsuite';
 
 export const Patients = () => {
-  const { data: patients, error, isLoading } = useSWR(`${apiBaseUrl}/patients`, feacher, {
-    refreshInterval: 1000,
-  }); 
+  const [patients, setPatients] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false); 
   const [patientToDelete, setPatientToDelete] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false); 
+
+  const loadPatients = () => {
+    setIsLoading(true);
+    api.get(`${apiBaseUrl}/patients`)
+      .then(response => {
+        setPatients(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    loadPatients();
+  }, []);
+
+  const addPatientToList = () => {
+    setIsModalOpen(false);
+    loadPatients();
+  };
 
   if (isLoading) {
     return <Styled.LoadingWrapper>Carregando lista de pacientes...</Styled.LoadingWrapper>;
@@ -25,10 +45,6 @@ export const Patients = () => {
   if (error) {
     return <Styled.LoadingWrapper>Erro ao carregar os pacientes</Styled.LoadingWrapper>;
   }
-
-  const addPatientToList = (newPatient) => {
-    patients((prevPatients) => [newPatient, ...prevPatients]);
-  };
 
   const handleDeletePatient = (patientId) => {
     setPatientToDelete(patientId);
@@ -39,6 +55,7 @@ export const Patients = () => {
     if (patientToDelete) {
       api.delete(`${apiBaseUrl}/patients/${patientToDelete}`)
         .then(() => {
+          loadPatients()
           setIsDeleteModalOpen(false)
         })
         .catch(error => {
@@ -77,23 +94,26 @@ export const Patients = () => {
           </tr>
         </thead>
         <tbody>
-          {patients.map(({ resource: patient }) => (
-              <tr key={patient.id}>
-                <Styled.Td>{patient.name[0].given} {patient.name[0].family}</Styled.Td>
-                <Styled.Td>{patient.telecom[0].value}</Styled.Td>
-                <Styled.Td>{format(utcToZonedTime(new Date(patient.birthDate), 'America/Sao_Paulo'), 'dd/MM/yyyy')}</Styled.Td>
-                <Styled.Td>
-                  <div style={{display: 'flex', justifyContent: 'space-around'}}>
-                    <DeleteOutlinedIcon
+        {patients.map(({ resource: patient }) => {
+          const date = patient.birthDate.split('-');
+          const formattedDate = `${date[2]}/${date[1]}/${date[0]}`;
+          return (
+            <tr key={patient.id}>
+              <Styled.Td>{patient.name[0].given} {patient.name[0].family}</Styled.Td>
+              <Styled.Td>{patient.telecom[0].value}</Styled.Td>
+              <Styled.Td>{formattedDate}</Styled.Td>
+              <Styled.Td>
+                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                  <DeleteOutlinedIcon
                     style={{ cursor: 'pointer' }}
                     onClick={() => handleDeletePatient(patient.id)}
-                    />
-                    <RemoveRedEyeIcon />
-                  </div>
-                </Styled.Td>
-              </tr>
-            ))
-          }
+                  />
+                  <RemoveRedEyeIcon />
+                </div>
+              </Styled.Td>
+            </tr>
+          );
+        })}
         </tbody>
       </Styled.Table>
       <Modal open={isDeleteModalOpen} onClose={handleCloseDeleteConfirmation}>
