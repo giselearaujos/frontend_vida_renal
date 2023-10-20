044c862be76ae688cfdaa747365432e92f290b03
Gisele Araujo Silva <gisele.araujosilva-ext@ab-inbev.com>
2023-10-19 22:42:28 -0300

    :sparkles: Added new name for variable

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const apiBaseUrl = import.meta.env.NEXT_PUBLIC_API_URL;
+// eslint-disable-next-line no-undef
+export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 console.log("🚀 ~ import.meta.env:", import.meta.env)
 
